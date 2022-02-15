c2d0e4c7c9812efe28f84dd8ca527519ee93088a
Jacob Hooser <jacob.hooser@yahoo.com>
2022-02-14 19:39:38 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(names, eName) {
+    const thanks = [];
+    for(let i = 0; i < names.length; i++) {
+        thanks.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return thanks;
+}
+function countDown(start) {
+    while (start >= 0) {
+        console.log(start);
+        start--;
+    }
+}
